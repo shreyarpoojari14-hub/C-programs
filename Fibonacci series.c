1b3a6b416b906698d8ca3ced41592f123032114a
Shreya <shreyarpoojari14@gmail.com>
2026-08-02 20:08:46 +0530

    Create Fibonacci series.c

---
+++ Fibonacci series.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+  int n,first=0,second=1,next,i;
+  printf("Enter number of terms:");
+  scanf("%d",&n);
+  printf("Fibonacci series:\n");
+  for(i=1;i<=n;i++)
+{
+printf("%d",first);
+next=first+second;
+first=second;
+second=next;
+}
+return 0;
+}
